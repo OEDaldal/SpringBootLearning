a3defc424d933724b5184d698aabff280bf91b47
Omer Daldal <179372592+OEDaldal@users.noreply.github.com>
2025-09-16 00:54:16 -0400

    Class creation

--- src/main/java/com/omeregedaldal/Application.java
+++ src/main/java/com/omeregedaldal/Application.java
@@ -2,12 +2,21 @@ package com.omeregedaldal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    // Rest Endpoint to verify the application is running
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World! Spring Boot is running.";
     }
 
 }
